f5b25725040a6417e0d7096de3804e4ded0caddd
Najat Hussein <nih6584@mavs.uta.edu>
2024-08-25 19:34:18 -0500

    User Input

--- P01/bonus/Hello.java
+++ P01/bonus/Hello.java
@@ -2,3 +2,6 @@ import java.util.Scanner;
 
 public class Hello {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
