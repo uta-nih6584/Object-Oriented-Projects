6d4b645e42e8deefcd791378212a22ba8f2a99bc
Najat Hussein <nih6584@mavs.uta.edu>
2024-08-25 19:25:00 -0500

    Print Message

--- P01/bonus/Hello.java
+++
@@ -1,6 +0,0 @@
-import java.util.Scanner;
-
-public class Example {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name: ");
