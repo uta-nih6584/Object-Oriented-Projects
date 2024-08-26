7ad9e26afe94aa8627bbc2490800b616181eb5d8
Najat Hussein <nih6584@mavs.uta.edu>
2024-08-25 19:35:00 -0500

    Print Statement

--- P01/bonus/Hello.java
+++ P01/bonus/Hello.java
@@ -5,3 +5,7 @@ public class Hello {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your name: ");
         String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+        scanner.close();
+    }
+}
