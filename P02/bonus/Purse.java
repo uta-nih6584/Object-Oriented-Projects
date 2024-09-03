31f03ca6983501c89ea1efea069c5fb8d1b297ca
Najat Hussein <nih6584@mavs.uta.edu>
2024-09-03 00:05:31 -0500

    adding coins

---
+++ P02/bonus/Purse.java
@@ -0,0 +1,10 @@
+public class Purse {
+	public static void main(String[] args) {
+		Coin[] coins = {
+ 	
+		new Coin(Denomination.Penny, 2015),	
+		new Coin(Denomination.Nickel, 2014),
+		new Coin(Denomination.Dime, 1990),	
+		new Coin(Denomination.Quarter, 2008),
+		new Coin(Denomination.Dime, 1982)
+	};
