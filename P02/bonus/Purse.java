6e32a689f0a04e361b2c29c0b77296550d862208
Najat Hussein <nih6584@mavs.uta.edu>
2024-09-03 00:10:08 -0500

    bulk

--- P02/bonus/Purse.java
+++ P02/bonus/Purse.java
@@ -8,3 +8,24 @@ public class Purse {
 		new Coin(Denomination.Quarter, 2008),
 		new Coin(Denomination.Dime, 1982)
 	};
+  double totalValue = 0;
+        int earliestYear = Integer.MAX_VALUE;
+        int latestYear = Integer.MIN_VALUE;
+
+        for (Coin coin : coins) {
+            totalValue += coin.getValue();
+            if (coin.getYear() < earliestYear) {
+                earliestYear = coin.getYear();
+            }
+            if (coin.getYear() > latestYear) {
+                latestYear = coin.getYear();
+            }
+        }
+
+        System.out.printf("You have $%.2f in coins between %d and %d%n", totalValue, earliestYear, latestYear);
+        System.out.println("Coins:");
+        for (Coin coin : coins) {
+            System.out.println(coin); 
+        }
+    }
+}
