4a98eb95d89fee85d95d8a66e766498b28769dbf
Najat Hussein <nih6584@mavs.uta.edu>
2024-09-02 23:05:44 -0500

    fixing spaces

--- P02/full_credit/Coin.java
+++ P02/full_credit/Coin.java
@@ -2,27 +2,21 @@ public class Coin {
 	private Denomination denomination;
 	private int year;
 
-
 	public Coin(Denomination denomination, int year) {
         this.denomination = denomination;
         this.year = year;
 
     }
 
-
     public double getValue() {
         return denomination.getValue();
 
-
     }
-
 
     public int getYear() {
         return year;
 
-
     }
-
 
     @Override
     public String toString() {
