0da00af5c91ace1421abc65286bcf08671511895
Najat Hussein <nih6584@mavs.uta.edu>
2024-09-02 20:26:53 -0500

    inital code

---
+++ P02/full_credit/Denomination.java
@@ -0,0 +1,18 @@
+public enum Denomination {
+	Penny(0.01), Nickle(0.05), Dime(0.10), Quarter(0.25);
+	
+	private Denomination(double value) {
+		this.value = value;
+	
+	}
+
+	public double getValue() {
+		return value;
+
+	}
+	
+	@Override
+	public String toString() {
+		return name();
+
+}
