eccc4f00cbfadd98b764aef06753276259a5b7d1
Najat Hussein <nih6584@mavs.uta.edu>
2024-09-10 00:40:57 -0500

    extreme bonus url validation

--- P03/Media.java
+++ P03/Media.java
@@ -1,10 +1,22 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Media {	
 	private String title;
 	private String url;
 
 public Media(String title, String url) {
 	this.title = title;
+	validateUrl(url);
         this.url = url;
+    }
+
+    private void validateUrl(String url) {
+        try {
+            new URL(url);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Invalid URL: " + url, e);
+        }
     }
 
     @Override
