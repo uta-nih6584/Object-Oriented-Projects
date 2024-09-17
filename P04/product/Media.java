3443ff6be3ea7c6e341a137c97b838c057b832f8
Najat Hussein <nih6584@mavs.uta.edu>
2024-09-16 22:21:18 -0500

    editing build.xml w apache license

--- P04/product/Media.java
+++ P04/product/Media.java
@@ -3,11 +3,26 @@ package product;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+/**
+ * Represents a media item with a title, URL, and associated points.
+ * 
+ * @author ##YOUR NAME##
+ * @version 0.2
+ * @since 0.2
+ */
 public class Media {
     private String title;
     private String url;
     private int points;
 
+    /**
+     * Constructs Media with the specified title, URL, and points.
+     * 
+     * @param title the title of the media item
+     * @param url the URL of the media item
+     * @param points the points associated with the media item
+     * @since 0.2
+     */
     public Media(String title, String url, int points) {
         this.title = title;
         validateUrl(url);
@@ -15,8 +30,25 @@ public class Media {
         this.points = points;
     }
 
+    /**
+     * Returns the points associated with this media item.
+     * 
+     * @return the points associated with the media item
+     * @since 0.2
+     */
     public int getPoints() {
         return points;
+    }
+
+    /**
+     * Returns a string representation of this media item.
+     * 
+     * @return a string representation of this media item
+     * @since 0.2
+     */
+    @Override
+    public String toString() {
+        return title + " (" + url + ", " + points + " points)";
     }
 
     private void validateUrl(String url) {
@@ -25,10 +57,5 @@ public class Media {
         } catch (MalformedURLException e) {
             throw new RuntimeException("Invalid URL: " + url, e);
         }
-    }
-
-    @Override
-    public String toString() {
-        return title + " (" + url + ", " + points + " points)";
     }
 }
