e0d5bb941f60ef80c1055cd43c962a4c1c68c09e
Najat Hussein <nih6584@mavs.uta.edu>
2024-10-25 14:24:37 -0500

    adding hello all png w 3 tests

---
+++ P08/hello_all.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	std::string name;
+	std::cout << "Enter your name: ";
+	std::getline(std::cin, name);
+	
+	std::cout << "Hello, " << name << "!" << std::endl;
+	return 0;
+}
